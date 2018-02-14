99ce6fc2b3e0ed4770cb8cffd93f05326f4bcec6
steff456 <steff456@hotmail.com>
2018-02-14 16:49:04 -0500

    Lab final

--- 03-Python/Answers/run.py
+++ 03-Python/Answers/run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import os.path as osp
 import matplotlib.pyplot as plt
@@ -8,13 +9,14 @@ import cv2
 import scipy.io as sio
 import time
 
-start_t = time.time()
-
 #Download of the database
 if not osp.exists('BSR'):
     print('------ Downloading database ------')
-    os.system('wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/BSR/BSR_bsds500.tgz')
-    os.system('tar -czvf archive.tar.gz BSR')
+    os.system('wget https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/BSR/BSR_bsds500.tgz')
+    print('------ Unzip database ------')
+    os.system('tar -czvf BSR_bsds500.tgz BSR')
+
+start_t = time.time()
 
 #Stores the database images directory
 #Dictionary, key: idImage, val: list of image, groundthruths
@@ -30,11 +32,12 @@ for root, dirs, files in os.walk('.', topdown=False):
             else:
                 seen[name[0]] = [root + '/' + f] 
 
+
 #Finds the number of images of the database 
 count = len(seen)
 
-#Generates a random number between 1 and the number of images from the dataset
-N = random.randint(1,count)
+#Generates a random number between 1 and 15
+N = random.randint(7,15)
 
 print('------ Showing ', N, ' images ------')
 
@@ -55,9 +58,11 @@ n= N
 #Transforms the N images to 256x256 and save them on the new folder
 print('------ Resize + New Folder ------')
 while(n>0):
+    #Actual image
+    num = random.randint(1,count)
     #Get the images and groundtruths
-    mat_act = sio.loadmat(seen[seen_keys[(n-1)]][0])['groundTruth']
-    img_act = cv2.imread(seen[seen_keys[(n-1)]][1]) 
+    mat_act = sio.loadmat(seen[seen_keys[num]][0])['groundTruth']
+    img_act = cv2.imread(seen[seen_keys[num]][1]) 
     gt = mat_act[0][0][0][0][0]
     gtt = mat_act[0][0][0][0][1]
     #Resize of the images
@@ -80,30 +85,35 @@ if osp.exists(name):
 
 np.save("dict.npy", final_d)
 
-print('------ Plotting images ------')
-#Only plot N<11 images for simplicity
-P = N%10 + 1
+#Number of images for plotting
+P = N
 
-#Plot of the images
+print('------ Plotting '+ str(P) +' image(s) ------')
+
+# Plot of the images
 fig = plt.figure()
 
 j = 1
 for i in range(1,P+1):
     #Original image
+    num = random.randint(0,(len(final_d)-1))
+    keys = list(final_d.keys())
+    key = keys[num]
     sub_fig = fig.add_subplot(P,3,j)
-    img_act = final_d[seen_keys[(i-1)]][0]
+    img_act = final_d[key][0]
     img_p = plt.imshow(img_act)
     j+=1
     #gt
     sub_fig = fig.add_subplot(P,3,j)
-    img_act = final_d[seen_keys[(i-1)]][1]
+    img_act = final_d[key][1]
     img_p = plt.imshow(img_act)    
     j+=1
     #gt
     sub_fig = fig.add_subplot(P,3,j)
-    img_act = final_d[seen_keys[(i-1)]][2]
+    img_act = final_d[key][2]
     img_p = plt.imshow(img_act)
     j+=1
+    final_d.pop(key, None)
 
 total_t = (time.time()-start_t)
 plt.show()
